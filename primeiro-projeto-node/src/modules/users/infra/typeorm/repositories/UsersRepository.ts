467993e48a0023e955c618bab85834a9078bf21e
luizAzevedo <lfaazevedo@hotmail.com>
2020-05-12 22:00:01 -0300

    Agendamento - Listagem de prestadores

--- primeiro-projeto-node/src/modules/users/infra/typeorm/repositories/UsersRepository.ts
+++ primeiro-projeto-node/src/modules/users/infra/typeorm/repositories/UsersRepository.ts
@@ -1,7 +1,8 @@
-import { getRepository, Repository } from 'typeorm';
+import { getRepository, Repository, Not } from 'typeorm';
 
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import ICreateUserDTO from '@modules/users/dtos/ICreateUserDTO';
+import IFindAllProvidersDTO from '@modules/users/dtos/IFindAllProvidersDTO';
 
 import User from '../entities/User';
 
@@ -24,6 +25,30 @@ class UsersRepository implements IUsersRepository {
     });
 
     return findUser;
+  }
+
+  public async findAllProviders({
+    except_user_id,
+  }: IFindAllProvidersDTO): Promise<User[]> {
+    // let users: User[];
+    // if (except_user_id) {
+    //   users = await this.ormRepository.find({
+    //     where: {
+    //       id: Not(except_user_id),
+    //     },
+    //   });
+    // } else {
+    //   users = await this.ormRepository.find();
+    // }
+    const users = except_user_id
+      ? await this.ormRepository.find({
+          where: {
+            id: Not(except_user_id),
+          },
+        })
+      : await this.ormRepository.find();
+
+    return users;
   }
 
   public async create(userData: ICreateUserDTO): Promise<User> {
